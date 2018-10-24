f7460835fecb068f829d1509d3c119ff9bef51b8
Brian Louie <brianalouie@gmail.com>
2018-10-24 16:21:56 -0700

    ran linter/prettier

--- src/components/Neighborhood.js
+++ src/components/Neighborhood.js
@@ -1,88 +1,69 @@
 import React from 'react';
 
-const Neighborhood = ({status, expand}) => {
-  return (
-    <div className='container'>
-      <div >
-        <section >
-          <h2 id='neighborhood-header' className={status ? 'header-toggle expanded' : 'header-toggle'} onClick={() => expand('neighborhood')}>
-            <span className='header-span'  >Neighborhood: SOMEPLACE</span>
-            {status ? <img className="chev" src='./down.png'/>
-              : <img className="chev" src='./up.png'/>}
-          </h2>
-          {status && (<div className='sub-container' id='neighborhood-active'>
-            <div >
-              <ul >
-                <li >
-                  <a href="#hdp-price-history" >Neighborhood Stuff</a>
-                </li>
-                <li >
-                  <a href="#hdp-tax-history" ></a>
-                </li>
-              </ul>
-              <div >
-                <div >
-                  <div >
-                    <table >
-                      <thead >
-                      <tr >
-                        <th >
-
-                        </th>
-                        <th >
-
-                        </th>
-                        <th >
-
-                        </th>
-                        <th >
-                        </th>
-                        <th >
-
-                        </th>
-                        <th >
-
-                        </th>
-                      </tr>
-                      </thead>
-                      <tbody >
-                      <tr >
-                        <td >
-
-                        </td>
-                        <td >
-
-                        </td>
-                        <td >
-
-                        </td>
-                        <td >
-                          --
-                        </td>
-                        <td >
-
-                        </td>
-                        <td >
-
-                        </td>
-                      </tr>
-                      </tbody>
-                    </table>
-                    <p >
-
-                    </p>
-                  </div>
-                  <img className='map' src='./map.png' />
-                </div>
-              </div>
-              <div >
+const Neighborhood = ({ status, expand }) => (
+  <div className="container">
+    <div>
+      <section>
+        <h2
+          id="neighborhood-header"
+          className={status ? 'header-toggle expanded' : 'header-toggle'}
+          onClick={() => expand('neighborhood')}
+        >
+          <span className="header-span">Neighborhood: SOMEPLACE</span>
+          {status ? (
+            <img className="chev" src="./down.png" />
+          ) : (
+            <img className="chev" src="./up.png" />
+          )}
+        </h2>
+        {status && (
+        <div className="sub-container" id="neighborhood-active">
+          <div>
+            <ul>
+              <li>
+                <a href="#hdp-price-history">Neighborhood Stuff</a>
+              </li>
+              <li>
+                <a href="#hdp-tax-history" />
+              </li>
+            </ul>
+            <div>
+              <div>
+                <div>
+                      <table>
+                        <thead>
+                          <tr>
+                            <th />
+                            <th />
+                            <th />
+                            <th />
+                            <th />
+                            <th />
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td />
+                            <td />
+                            <td />
+                            <td>--</td>
+                            <td />
+                            <td />
+                          </tr>
+                        </tbody>
+                      </table>
+                      <p />
+                    </div>
+                <img className="map" src="./map.png" />
               </div>
             </div>
-          </div>)}
-        </section>
-      </div>
+            <div />
+          </div>
+        </div>
+        )}
+      </section>
     </div>
-  );
-};
+  </div>
+);
 
 export default Neighborhood;
